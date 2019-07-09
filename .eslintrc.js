fcfd7666ed0d8dd9049f71eb6c6e78495d553e13
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-09 14:04:35 -0300

    new config added

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
           }
       ],
       'import/prefer-default-export': 'off',
+      'no-param-reassing': 'off',
       'no-console': ["error", { allor: ["tron"] }]
   },
 };
