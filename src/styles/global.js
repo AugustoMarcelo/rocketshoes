eb10d72c3fbd1f441ce741a60722f69de84342bc
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-02 14:59:33 -0300

    Moved font import to index.html

--- src/styles/global.js
+++ src/styles/global.js
@@ -4,31 +4,30 @@ import 'react-toastify/dist/ReactToastify.css';
 import background from '../assets/images/background.svg';
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
 
-    * {
-        margin: 0;
-        padding: 0;
-        outline: 0;
-        box-sizing: border-box;
-    }
+  body {
+    background: #42A5F5 url(${background}) no-repeat center top;
+    -webkit-font-smoothing: antialiased;
+  }
 
-    body {
-        background: #42A5F5 url(${background}) no-repeat center top;
-        -webkit-font-smoothing: antialiased;
-    }
+  body, input, button {
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+  }
 
-    body, input, button {
-        font: 14px Roboto, sans-serif;
-    }
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+  }
 
-    #root {
-        max-width: 1020px;
-        margin: 0 auto;
-        padding: 0 20px 50px;
-    }
-
-    button {
-        cursor: pointer;
-    }
+  button {
+    cursor: pointer;
+  }
 `;
