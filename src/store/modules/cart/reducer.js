feec39974af51dc737d0e843c37771e65d4a99c9
AugustoMarcelo <mrclgst10@gmail.com>
2020-05-02 22:38:04 -0300

    Eslint fixing

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,9 @@
 import produce from 'immer';
 
 const INITIAL_STATE = {
-  products: JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY)) || [],
+  products:
+    JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY)) ||
+    [],
   adding: [],
 };
 
