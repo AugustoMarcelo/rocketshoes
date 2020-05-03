ad890e7cb0764eb7fd03153f2a669369eadebb06
Marcelo Augusto <mrclgst10@gmail.com>
2020-05-02 22:57:06 -0300

    Update reducer.js

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -2,8 +2,7 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
   products:
-    JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY)) ||
-    [],
+    JSON.parse(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_KEY) || []),
   adding: [],
 };
 
